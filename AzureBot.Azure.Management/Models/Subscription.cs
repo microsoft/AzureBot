0ebfb5939041583ed548a7d51d8944f37f7c14ad
Fernando Tubio <fernando.tubio@southworks.net>
2016-05-12 19:35:41 -0300

    Udpated mock AzureRepository to handle virtual machine resources

--- AzureBot.Azure.Management/Models/Subscription.cs
+++ AzureBot.Azure.Management/Models/Subscription.cs
@@ -1,8 +1,5 @@
-﻿using System;
-
-namespace AzureBot.Azure.Management.Models
+﻿namespace AzureBot.Azure.Management.Models
 {
-    [Serializable]
     public class Subscription
     {
         public string DisplayName { get; set; }
