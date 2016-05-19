324796d5e98b167196a3eb587a6d37f1d7ab7367
Fernando Tubio <fernando.tubio@southworks.net>
2016-05-19 15:49:17 -0300

    Switched to IDialogContext.MakeMessage for generating the notification message

--- AzureBot/Dialogs/DialogExtensions.cs
+++ AzureBot/Dialogs/DialogExtensions.cs
@@ -11,28 +11,20 @@
     {
         public static void NotifyLongRunningOperation<T>(this Task<T> operation, IDialogContext context, Func<T, IDialogContext, string> handler)
         {
-            ResumptionCookie resumptionCookie;
-
             ////context.PostAsync("This operation may take some time. You may continue with other tasks. You'll be notified when the operation is complete.");
-            if (context.PerUserInConversationData.TryGetValue(ContextConstants.PersistedCookieKey, out resumptionCookie))
-            {
-                operation.ContinueWith(
-                    async (t, ctx) =>
-                    {
-                        var reply = resumptionCookie.GetMessage();
-                        var to = reply.To;
-                        reply.To = reply.From;
-                        reply.From = to;
-                        reply.Text = handler(t.Result, (IDialogContext)ctx);
+            operation.ContinueWith(
+                async (t, ctx) =>
+                {
+                    var reply = ((IDialogContext)ctx).MakeMessage(); 
+                    reply.Text = handler(t.Result, (IDialogContext)ctx);
 
-                        using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, reply))
-                        {
-                            var client = scope.Resolve<IConnectorClient>();
-                            await client.Messages.SendMessageAsync(reply);
-                        }
-                    }, 
-                    context);
-            }
+                    using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, reply))
+                    {
+                        var client = scope.Resolve<IConnectorClient>();
+                        await client.Messages.SendMessageAsync(reply);
+                    }
+                }, 
+                context);
         }
     }
 }
