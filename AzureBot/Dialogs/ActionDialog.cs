1676f3a955a8a04b89b46f1bb8d07769389db876
Ezequiel Jadib <ezequiel.jadib@southworks.net>
2016-06-16 13:01:44 -0300

    Improve message when no automation accounts are available.

--- AzureBot/Dialogs/ActionDialog.cs
+++ AzureBot/Dialogs/ActionDialog.cs
@@ -223,7 +223,7 @@
             }
             else
             {
-                await context.PostAsync($"No automations accounts were found in the current subscription.");
+                await context.PostAsync($"No automations accounts were found in the current subscription. Please create an Azure Automation account or switch to a subscription which has an Automation account in it.");
                 context.Wait(this.MessageReceived);
             }
         }
