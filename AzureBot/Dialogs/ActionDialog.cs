9d625e8b39c9aec67caed82952fd792b096e0065
Ezequiel Jadib <ezequieljadib@hotmail.com>
2016-06-23 18:30:41 -0300

    Add show description messages to Parameters and List runbooks (#52)
    * Support for showing the description of a runbook. Issue #43
    * Add messages to inform users to use show description
    * Update help

--- AzureBot/Dialogs/ActionDialog.cs
+++ AzureBot/Dialogs/ActionDialog.cs
@@ -43,7 +43,7 @@
             message += $"* List, Switch and Select an Azure subscription\n";
             message += $"* List, Start, Shutdown (power off your VM, still incurring compute charges), and Stop (deallocates your VM, no charges) your virtual machines\n";
             message += $"* List your automation accounts and your runbooks\n";
-            message += $"* Start a runbook and get the status of the runbook jobs\n";
+            message += $"* Start a runbook, get the description of a runbook, get the status and ouput of automation jobs\n";
             message += $"* Logout to sign out from Azure\n\n";
             message += $"Please type **login** to interact with me for the first time.";
             
@@ -235,7 +235,8 @@
                             return current += singleAutomationAccount ? innerRunbooksText : $"\n\r {next.AutomationAccountName}" + innerRunbooksText;
                         });
 
-                    await context.PostAsync($"{messageText}:\r\n {runbooksText}");
+                    var showDescriptionText = "Type **show runbook <name> description** to get details on any runbook.";
+                    await context.PostAsync($"{messageText}:\r\n {runbooksText} \r\n\r\n {showDescriptionText}");
                 }
                 else
                 {
@@ -650,8 +651,13 @@
                 return;
             }
 
+            var formState = new RunbookParameterFormState(nextRunbookParameter.IsMandatory, nextRunbookParameter.Position == 0, runbookFormState.SelectedRunbook.RunbookName)
+            {
+                ParameterName = nextRunbookParameter.ParameterName
+            };
+
             var form = new FormDialog<RunbookParameterFormState>(
-                new RunbookParameterFormState(nextRunbookParameter.IsMandatory) { ParameterName = nextRunbookParameter.ParameterName },
+                formState,
                 EntityForms.BuildRunbookParametersForm,
                 FormOptions.PromptInStart);
 
