e2b872113664831c14c81d78a6ca8ce86f622712
Thiago Almeida <thalme@microsoft.com>
2016-06-24 09:51:48 -0700

    add a console app to help setting user context, and initial functional test framework with one test

---
+++ AzureBot.ConsoleConversation/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Bot.Connector.DirectLine;
+using Microsoft.Bot.Connector.DirectLine.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBot.ConsoleConversation
+{
+    class Program
+    {
+        public static string ConversationId = ConfigurationManager.AppSettings["ConversationId"];
+        public static string DirectLineToken = ConfigurationManager.AppSettings["DirectLineToken"];
+        public static string AppId = ConfigurationManager.AppSettings["AppId"];
+        public static string FromUser = ConfigurationManager.AppSettings["FromUser"];
+        static void Main(string[] args)
+        {
+            StartBotConversation().Wait();
+        }
+
+        static async Task StartBotConversation()
+        {
+            DirectLineClient client = new DirectLineClient(DirectLineToken);
+            string strWatermark = null;
+            MessageSet msgs = await client.Conversations.GetMessagesAsync(ConversationId, strWatermark);
+            strWatermark = msgs?.Watermark;
+            while (true)
+            {
+                Console.Write("Command>");
+                string strInput = Console.ReadLine().Trim();
+
+                if (strInput.ToLower() == "exit")
+                    break;
+                else
+                {
+
+                    if (strInput.Length > 0)
+                    {
+                        Message aMsg = new Message
+                        {
+                            FromProperty = FromUser,
+                            Text = strInput
+                        };
+
+                        Debug.WriteLine($"Sending Message: {strInput}");
+                        await client.Conversations.PostMessageAsync(ConversationId, aMsg);
+                        msgs = await client.Conversations.GetMessagesAsync(ConversationId, strWatermark);
+                        strWatermark = msgs?.Watermark;
+
+                        Debug.WriteLine($"Received {msgs.Messages.Count}");
+                        foreach (Message m in msgs.Messages)
+                        {
+                            Debug.WriteLine(m.FromProperty);
+                            Debug.WriteLine(m.Text);
+                            Debug.WriteLine("------------------------------");
+                            if ("azurebot" == m.FromProperty)
+                                Console.WriteLine(m.Text);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AzureBot.Tests/VMTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace AzureBot.Tests
+{
+    /// <summary>
+    /// Tests all VM commands
+    /// </summary>
+    [TestClass]
+    public class VMTests
+    {
+        [TestMethod]
+        public void CanListVMs()
+        {
+            string expected = "Available VMs are";
+            General.botHelper.SendMessage("list vms");
+            string lastMessage = General.botHelper.LastMessageFromBot().Result;
+            Assert.IsTrue(lastMessage.StartsWith(expected));
+        }
+    }
+}
