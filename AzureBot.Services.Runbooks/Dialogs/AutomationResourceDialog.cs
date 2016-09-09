344f64e94fe76f4b445bdda9d7412220e22e6555
Eric Hexter <eric@quarterspot.com>
2016-09-08 23:28:24 -0500

    prototype add dialog factory to remove depenendy on resources in webapi process #109

---
+++ AzureBot.Services.Runbooks/Dialogs/AutomationResourceDialog.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+
+namespace AzureBot.Dialogs
+{
+    public class AutomationResourceDialog:IResourceDialog
+    {
+        public bool CanHandle(string query)
+        {
+            return query.ToLowerInvariant().Contains("job") | query.ToLowerInvariant().Contains("runbook");
+        }
+
+        public IDialog<string> Create()
+        {
+            return new AutomationDialog();
+        }
+    }
+}
